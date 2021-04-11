87bd1f8f9db20e9a1272fd84804370b2fd577bf0
wansyazlina <syazlina2008@gmail.com>
2021-04-11 13:09:49 +0800

    added svt-numpad keymap

--- keyboards/dumbpad/config.h
+++ keyboards/dumbpad/config.h
@@ -19,16 +19,32 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0913
-#define MANUFACTURER    imchipwood
-#define PRODUCT         dumbpad
+// #define VENDOR_ID       0xFEED
+// #define PRODUCT_ID      0x0913
+// #define MANUFACTURER    imchipwood
+ #define PRODUCT         dumbpad
 
 /* Column/Row IO definitions */
-#define DIODE_DIRECTION COL2ROW
+// #define DIODE_DIRECTION COL2ROW
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
+ #define DEBOUNCE 5
 
 /* Reduce tapdance required taps from 5 to 2 */
 #define TAPPING_TOGGLE 2
+
+/* LED layer indicators */
+
+// Using external LED for layers indicator
+ #define LAYER_INDICATOR_LED_0 B6 // Red
+ #define LAYER_INDICATOR_LED_1 B1 // Blue or Green
+
+ // On-board LED for debugging
+
+ #define LAYER_INDICATOR_LED_2 B0
+#define LAYER_INDICATOR_LED_3 D5
+
+#define RGB_DI_PIN B3
+#ifdef RGB_DI_PIN
+#define RGBLED_NUM 4
+#endif
